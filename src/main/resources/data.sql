6b147c3efd9050e3c195ba723e50eeba0083b587
isExample <yeji0331@naver.com>
2024-04-01 10:24:54 +0900

    [Feature] 초기 데이터 id 값 미지정
    Auto-increment로 수정함에 따라 초기 데이터 insert문에서 id를 지정하지 않도록 합니다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
-INSERT INTO `MEMBER` (`id`, `email`, `name`, `password`, `phone`, `address`, `balance`, `createdAt`, `updatedAt`) VALUES
-    ('1', 'member@example.com', '김멤버', '1234', '123-456-7890', '서울시 중구', 100000, NOW(), NOW());
+INSERT INTO `MEMBER` (`email`, `name`, `password`, `phone`, `address`, `balance`, `createdAt`, `updatedAt`) VALUES
+    ('member@example.com', '김멤버', '1234', '123-456-7890', '서울시 중구', 100000, NOW(), NOW());
 
-INSERT INTO `PRODUCT` (`id`, `name`, `description`, `price`, `stock`, `createdAt`) VALUES
-    ('1', '제품1', '더미 제품1 입니다.', 7500, 100, NOW());
+INSERT INTO `PRODUCT` (`name`, `description`, `price`, `stock`, `createdAt`) VALUES
+    ('제품1', '더미 제품1 입니다.', 7500, 100, NOW());
 
-INSERT INTO `ORDERS` (`id`, `memberId`, `status`, `totalPrice`, `createdAt`, `updatedAt`) VALUES
-    ('1', '1', 'COMPLETE', 7500, NOW(), NOW());
+INSERT INTO `ORDERS` (`memberId`, `status`, `totalPrice`, `createdAt`, `updatedAt`) VALUES
+    ('1', 'COMPLETE', 7500, NOW(), NOW());
 
 INSERT INTO `ORDER_DETAIL` (`orderId`, `productId`, `price`, `quantity`) VALUES
     ('1', '1', 7500, 1);
 
-INSERT INTO `PAYMENT` (`id`, `orderId`, `amount`, `status`, `createdAt`, `updatedAt`) VALUES
-    ('1', '1', 7500, 'COMPLETE', NOW(), NOW());
+INSERT INTO `PAYMENT` (`orderId`, `amount`, `status`, `createdAt`, `updatedAt`) VALUES
+    ('1', 7500, 'COMPLETE', NOW(), NOW());
 
 INSERT INTO `CART` (`memberId`, `productId`, `quantity`, `createdAt`) VALUES
     ('1', '1', 1, NOW());
