fa4857e7b0778a0140df19fe6e021edfbf45c7c3
isExample <yeji0331@naver.com>
2024-04-17 14:49:23 +0900

    [Refactor] RequestBody DTO immutable하도록 개선
    RequestBody DTO를 명시적으로 Immutable하게 개선했습니다.

--- src/main/java/com/flab/order/domain/dto/MemberRequest.java
+++ src/main/java/com/flab/order/domain/dto/MemberRequest.java
@@ -1,19 +1,27 @@
 package com.flab.order.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 public class MemberRequest {
 
-    @NoArgsConstructor
-    @Getter
+    @Value
     public static class Login{
         @NotBlank(message = "이메일을 입력해 주세요.")
         @Email(message = "유효하지 않은 이메일 형식입니다.")
-        private String email;
+        String email;
         @NotBlank(message = "비밀번호를 입력해 주세요.")
-        private String password;
+        String password;
+
+        @JsonCreator
+        public Login(@JsonProperty("email") String email, @JsonProperty("password") String password) {
+            this.email = email;
+            this.password = password;
+        }
     }
 }
