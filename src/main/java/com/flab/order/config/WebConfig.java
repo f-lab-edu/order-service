06ececdc1eb08a8b6e9c6e45c55f7e3e38232461
isExample <yeji0331@naver.com>
2024-04-17 14:28:31 +0900

    [Feature] Interceptor와 Argument Resolver를 MVC 설정에 등록
    로그인 인증을 위한 커스텀 Interceptor와 Argument Resolver를 등록했습니다.

--- src/main/java/com/flab/order/config/WebConfig.java
+++ src/main/java/com/flab/order/config/WebConfig.java
@@ -1,2 +1,30 @@
-package com.flab.order.config;public class WebConfig {
+package com.flab.order.config;
+
+import com.flab.order.global.interceptor.MemberAuthorizationInterceptor;
+import com.flab.order.global.resolver.LoginMemberResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final MemberAuthorizationInterceptor memberAuthorizationInterceptor;
+    private final LoginMemberResolver loginMemberResolver;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) { // 인터셉터 MVC 구성에 추가
+        registry.addInterceptor(memberAuthorizationInterceptor)
+                .addPathPatterns("/orders/**"); // 인터셉터 적용할 경 등록
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberResolver);
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    }
 }
