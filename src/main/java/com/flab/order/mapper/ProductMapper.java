9a2baa6de547f6a3d1efcc8c8ce30e6784c817e2
isExample <yeji0331@naver.com>
2024-04-21 14:33:57 +0900

    [Feature] 상품 재고 감소 mapper 구현
    상품 재고를 감소하는 mapper 인터페이스 메서드와 sql을 구현했습니다.

--- src/main/java/com/flab/order/mapper/ProductMapper.java
+++ src/main/java/com/flab/order/mapper/ProductMapper.java
@@ -1,12 +1,16 @@
 package com.flab.order.mapper;
 
 
+import com.flab.order.domain.entity.Cart;
 import com.flab.order.domain.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface ProductMapper {
     Optional<Product> findById(Long id);
+    int updateAllByStock(List<Cart> cartItemList);
 }
