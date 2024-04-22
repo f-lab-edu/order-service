d666c1e010616e4d0fea7185aa5f63efe56b1008
isExample <yeji0331@naver.com>
2024-04-22 20:08:30 +0900

    [Feature] 동시성 테스트 시나리오 2 구현
    상품 재고 관련 동시성 테스트 시나리오 2를 구현했습니다.

--- src/test/java/com/flab/order/service/OrderServiceTest.java
+++ src/test/java/com/flab/order/service/OrderServiceTest.java
@@ -68,4 +68,34 @@ public class OrderServiceTest {
 
         assertEquals(1, successCount, "상품 재고 동시성 테스트에 실패했습니다.");
     }
+
+    @Test
+    @DisplayName("[시나리오 2] 2명의 회원이 동시에 재고가 5인 상품을 각각 3개, 4개 주문 시도") // 상품 재고가 -1 이하가 되어서는 안됨
+    void testOrderConcurrency2() {
+        Long memberId1 = 4L;
+        Long memberId2 = 5L;
+
+        Future<?> future1 = executorService.submit(() -> orderService.orderCartItems(memberId1));
+        Future<?> future2 = executorService.submit(() -> orderService.orderCartItems(memberId2));
+
+        int successCount = 0;
+
+        try {
+            future1.get();
+            logger.info("[TEST] 회원 4가 성공적으로 상품 3을 주문했습니다.");
+            successCount++;
+        } catch (Exception e) {
+            logger.info("[TEST] 회원 4가 상품 3 주문에 실패했습니다. " + e.getMessage());
+        }
+
+        try {
+            future2.get();
+            logger.info("[TEST] 회원 5가 성공적으로 상품 3을 주문했습니다.");
+            successCount++;
+        } catch (Exception e) {
+            logger.info("[TEST] 회원 5가 상품 3 주문에 실패했습니다. " + e.getMessage());
+        }
+
+        assertEquals(1, successCount, "상품 재고 동시성 테스트에 실패했습니다.");
+    }
 }
